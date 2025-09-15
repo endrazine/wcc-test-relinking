47041e2b7b1b5f60a26de9d4edfa279ba7ffc398
Jonathan Brossard <endrazine@gmail.com>
2025-09-15 08:43:14 +0200

    Improved Dockerfiles for Ubuntu 20.04, 18.04 and 16.04, that exhibited compilation problems with cargo.

--- build-all.sh
+++ build-all.sh
@@ -2,9 +2,9 @@
 
 docker buildx build --load . -t wcc:14.04 -f Dockerfile.14.04
 
-docker buildx build --load . -t wcc:16.04 --build-arg VERSION=16.04
-docker buildx build --load . -t wcc:18.04 --build-arg VERSION=18.04
-docker buildx build --load . -t wcc:20.04 --build-arg VERSION=20.04
+docker buildx build --load . -f Dockerfile-16.04 -t wcc:16.04 --build-arg VERSION=16.04
+docker buildx build --load . -f Dockerfile-18.04 -t wcc:18.04 --build-arg VERSION=18.04
+docker buildx build --load . -f Dockerfile-20.04 -t wcc:20.04 --build-arg VERSION=20.04
 docker buildx build --load . -t wcc:22.04 --build-arg VERSION=22.04
 docker buildx build --load . -t wcc:24.04 --build-arg VERSION=24.04
 
